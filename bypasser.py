74ba8792cc6a0e85560c7505da2e45a5549400d4
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-02-19 17:07:51 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2012,6 +2012,28 @@ def tnshort(url):
     except BaseException:
         return "Something went wrong :("
 
+#####################################################################################################
+# https://publicearn.com
+
+def tnshort(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://go.tnshort.net/"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://jrlinks.in/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(9)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return str(r.json()["url"])
+    except BaseException:
+        return "Something went wrong :("
 
 #####################################################################################################
 # shrink
