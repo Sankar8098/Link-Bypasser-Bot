96f87642a1807ae3f6bf49e449b6af81dc432fe2
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-02-19 17:10:17 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2021,7 +2021,7 @@ def tnshort(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://jrlinks.in/"
+    ref = "https://starxinvestor.com"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
